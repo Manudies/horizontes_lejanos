de0be73b4c7c78f25d0a2dd0c2c18973ec3ce130
Manudies <manudies@gmail.com>
2024-06-10 16:06:21 +0200

    router

--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -1,21 +1,19 @@
 import {Router} from "express";
 
 import userRouter from "./userRouter.js";
-import projectRouter from "./projectRouter.js";
-import taskRouter from "./taskRouter.js";
+import tripRouter from "./tripRouter.js";
+import favoriteRouter from "./favoritesRouter.js";
 import authRouter from "./authRouter.js";
-import invitationRouter from "./invitationRouter.js";
 
 import { isAuthenticated,isAdmin } from "../middlewares/authMiddleware.js";
 
 const router  =  Router();
 
 router.get("/",(req,res)=>{
-    res.json({data:"hello api"});
+    res.json({data:"Hola API"});
 })
 router.use("/users",isAuthenticated,userRouter);
-router.use("/projects",isAuthenticated,projectRouter);
-router.use("/tasks",taskRouter);
+router.use("/trips",isAuthenticated,tripRouter);
+router.use("/favorites",favoriteRouter);
 router.use("/",authRouter);
-router.use("/invitations",isAuthenticated,invitationRouter);
 export default router;
