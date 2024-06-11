6b9f730c98d7089f386f0e1204290cc19200b9a1
Manudies <manudies@gmail.com>
2024-06-11 11:47:41 +0200

    Prueba de meter usuarios postman

--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -9,7 +9,8 @@ router.get("/",userApiController.getAll);
 router.get("/byproperty",userApiController.getByProperty);
 router.get("/bytoken",isAuthenticated,userApiController.getByToken);
 router.get("/:id",userApiController.getById);
-router.post("/",isAdmin,userApiController.create);
+// router.post("/",isAdmin,userApiController.create); para usar desde el front
+router.post("/",userApiController.create);
 router.put("/:id",isAdmin,userApiController.update);
 router.delete("/:id",isAdmin,userApiController.remove);
 
