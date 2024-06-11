7b20d081be644a2086ca7262449d439e0a5d234c
onluani <anitsitska@gmail.com>
2024-06-11 16:03:33 +0200

    navbar sin links

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
@@ -12,27 +12,27 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
+        {/* <Link to="/" className="navbar-logo"> */}
           Logo
-        </Link>
+        {/* </Link> */}
         <div className={isMenuOpen ? 'menu-icon open' : 'menu-icon'} onClick={toggleMenu}>
           <i className={isMenuOpen ? 'fas fa-times' : 'fas fa-bars'}></i>
         </div>
         <ul className={isMenuOpen ? 'nav-menu active' : 'nav-menu'}>
           <li className="nav-item">
-            <Link to="/" className="nav-links" onClick={toggleMenu}>
+            {/* <Link to="/" className="nav-links" onClick={toggleMenu}> */}
               Home
-            </Link>
+            {/* </Link> */}
           </li>
           <li className="nav-item">
-            <Link to="/about" className="nav-links" onClick={toggleMenu}>
+            {/* <Link to="/about" className="nav-links" onClick={toggleMenu}> */}
               About
-            </Link>
+            {/* </Link> */}
           </li>
           <li className="nav-item">
-            <Link to="/trips" className="nav-links" onClick={toggleMenu}>
+            {/* <Link to="/trips" className="nav-links" onClick={toggleMenu}> */}
               Trips
-            </Link>
+            {/* </Link> */}
           </li>
         </ul>
       </div>
