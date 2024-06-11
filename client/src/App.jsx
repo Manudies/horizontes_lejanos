315640dbd892a439a9bde2195d67f8924dc56e90
onluani <anitsitska@gmail.com>
2024-06-11 15:14:02 +0200

    navbar done but not tested

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import router from './router.jsx'
 import { RouterProvider } from 'react-router-dom'
 import UserContext from './context/userContext'
 import './App.css'
+import Navbar from './components/navbar/Navbar.jsx'
 
 
 function App() {
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/trips">Trips</Link></li>
-      </ul>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          Logo
+        </Link>
+        <div className={isMenuOpen ? 'menu-icon open' : 'menu-icon'} onClick={toggleMenu}>
+          <i className={isMenuOpen ? 'fas fa-times' : 'fas fa-bars'}></i>
+        </div>
+        <ul className={isMenuOpen ? 'nav-menu active' : 'nav-menu'}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={toggleMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-links" onClick={toggleMenu}>
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/trips" className="nav-links" onClick={toggleMenu}>
+              Trips
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
