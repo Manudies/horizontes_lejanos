ff40939717de7598c4a4689aec17241628438cb6
bego <begofv13@gmail.com>
2024-06-17 16:56:06 +0200

    tarjetas bienvenidas

--- client/src/components/bienvenida/bienvenida.jsx
+++ client/src/components/bienvenida/bienvenida.jsx
@@ -43,19 +43,41 @@ const Main = () => {
           <div className="recomendaciones">
               <h2>¿Por qué contratar tu viaje con nosotros?</h2>
               <p>Elegir Horizontes Lejanos para contratar tu viaje puede ser una excelente decisión por varias razones:</p>
-                <ul>
-                  <li>1. Experiencia y Conocimiento: Horizontes Lejanos podría contar con años de experiencia en la industria del turismo, lo que les permite ofrecer asesoramiento experto y recomendaciones personalizadas basadas en tus intereses y necesidades de viaje.</li>
-                  <li>2. Destinos Exóticos y Exclusivos: Esta agencia puede especializarse en destinos lejanos y exóticos, brindándote la oportunidad de explorar lugares únicos y menos convencionales que quizás no encontrarías fácilmente por tu cuenta.</li>
-                  <li>3. Paquetes Personalizados: Ofrecen la posibilidad de personalizar los paquetes de viaje, asegurando que cada detalle se ajuste a tus preferencias, desde el tipo de alojamiento hasta las actividades y excursiones.</li>
-                  <li>4. Soporte Integral: Proveen un servicio integral que incluye asistencia antes, durante y después del viaje, asegurando que cualquier inconveniente sea resuelto de manera eficiente y rápida.</li>
-                  <li>5. Seguridad y Tranquilidad: Viajar con una agencia reconocida te brinda la tranquilidad de saber que cuentas con el respaldo de profesionales, lo que puede ser especialmente importante en destinos remotos donde podrías enfrentar barreras idiomáticas o culturales.</li>
-                  <li>6.Ofertas y Promociones: Pueden tener acceso a ofertas exclusivas y promociones especiales gracias a sus alianzas con proveedores de servicios turísticos, lo que te puede permitir obtener mejores precios y beneficios adicionales.</li>
-                  <li>7. Sostenibilidad y Responsabilidad: Algunas agencias, como Horizontes Lejanos, pueden estar comprometidas con el turismo sostenible, promoviendo prácticas responsables que beneficien a las comunidades locales y minimicen el impacto ambiental.</li>
-                  <li>8. Testimonios y Reputación: Revisar testimonios y opiniones de otros viajeros puede darte una idea de la calidad del servicio que ofrecen. Una buena reputación en la industria es indicativa de la satisfacción de sus clientes.</li>
-                  <li>9. Facilidad de Pago: Pueden ofrecer diversas opciones de pago y financiamiento, lo que facilita la planificación y realización de tu viaje sin preocuparte por el desembolso inmediato de grandes sumas de dinero.</li>
-                </ul>
+              <div className='contenedor-tarjetas'>
+                <div class="tarjeta">
+                  <h2>Experiencia y Conocimiento</h2>
+                  <p>Horizontes Lejanos podría contar con años de experiencia en la industria del turismo, lo que les permite ofrecer asesoramiento experto y recomendaciones personalizadas basadas en tus intereses y necesidades de viaje.</p>
+                </div>
+                <div class="tarjeta">
+                  <h2>Destinos Exóticos y Exclusivos</h2>
+                  <p>Esta agencia puede especializarse en destinos lejanos y exóticos, brindándote la oportunidad de explorar lugares únicos y menos convencionales que quizás no encontrarías fácilmente por tu cuenta.</p>
+                </div>
+                <div class="tarjeta">
+                  <h2>Paquetes Personalizados</h2>
+                  <p>Ofrecen la posibilidad de personalizar los paquetes de viaje, asegurando que cada detalle se ajuste a tus preferencias, desde el tipo de alojamiento hasta las actividades y excursiones.</p>
+                </div>
+                <div class="tarjeta">
+                    <h2>Soporte Integral</h2>
+                    <p>Proveen un servicio integral que incluye asistencia antes, durante y después del viaje, asegurando que cualquier inconveniente sea resuelto de manera eficiente y rápida.</p>
+                </div>
+                <div class="tarjeta">
+                    <h2>Seguridad y Tranquilidad</h2>
+                    <p>Viajar con una agencia reconocida te brinda la tranquilidad de saber que cuentas con el respaldo de profesionales, lo que puede ser especialmente importante en destinos remotos donde podrías enfrentar barreras idiomáticas o culturales.</p>
+                </div>
+                <div class="tarjeta">
+                    <h2>Sostenibilidad y Responsabilidad</h2>
+                    <p>Algunas agencias, como Horizontes Lejanos, pueden estar comprometidas con el turismo sostenible, promoviendo prácticas responsables que beneficien a las comunidades locales y minimicen el impacto ambiental.</p>
+                </div>
+                <div class="tarjeta">
+                    <h2>Testimonios y Reputación</h2>
+                    <p>Revisar testimonios y opiniones de otros viajeros puede darte una idea de la calidad del servicio que ofrecen. Una buena reputación en la industria es indicativa de la satisfacción de sus clientes.</p>
+                </div>
+                <div class="tarjeta">
+                    <h2>Facilidad de Pago</h2>
+                    <p>Pueden ofrecer diversas opciones de pago y financiamiento, lo que facilita la planificación y realización de tu viaje sin preocuparte por el desembolso inmediato de grandes sumas de dinero.</p>
+                </div>
+              </div>
               <p>Al elegir una agencia como Horizontes Lejanos, te aseguras de tener una experiencia de viaje bien planificada, segura y adaptada a tus expectativas, lo que puede transformar un simple viaje en una experiencia inolvidable.</p>
-
           </div>
         <div>
           <Carrusel />
@@ -64,8 +86,8 @@ const Main = () => {
           <ContactForm />
         </div>
 
-      </section>
-    </main>
+      </section >
+    </main >
   );
 }
 
