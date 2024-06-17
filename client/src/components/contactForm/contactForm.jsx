b7fe1e8eb33420833be7b4cf39665402d5ac5141
Lander <lander.arbelaiz@gmail.com>
2024-06-17 13:45:28 +0200

    FIX : in bienvenida.jsx carrusel and contactForm sitched possitions

--- client/src/components/contactForm/contactForm.jsx
+++ client/src/components/contactForm/contactForm.jsx
@@ -6,29 +6,29 @@ function ContactForm() {
             <h2>
                 Contáctanos
             </h2>
-            <Form.Group className="mb-3" controlId="name">
+            <Form.Group className="form-group" controlId="name">
                 <Form.Label className='label'>Nombre y Apellidos: </Form.Label>
                 <Form.Control type="text" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="mail">
+            <Form.Group className="form-group" controlId="mail">
                 <Form.Label className='label'>Tu correo electrónico: </Form.Label>
                 <Form.Control type="email" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="phone">
+            <Form.Group className="form-group" controlId="phone">
                 <Form.Label className='label'>Número de Teléfono: </Form.Label>
                 <Form.Control type="text" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="enquiry">
+            <Form.Group className="form-group" controlId="enquiry">
                 <Form.Label className='label'>Tu consulta: </Form.Label>
                 <Form.Control as="textarea" rows={3} />
             </Form.Group>
-            <div className="mb-3">
+            <div className="form-group">
                 <Form.Check
                     id={`Privacy`}
                     label={`He leído y Aceptado la Politica de Privacidad.`}
                 />
             </div>
-            <div className="mb-3">
+            <div className="form-group">
                 <Form.Check
                     id={`TermsCons`}
                     label={`He leído y Aceptado los Términos y Condiciones.`}
